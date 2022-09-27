8a5276c81aeb5ddabb04adfe31860215818fa567
Deepraj Pradhan <deeprajpradhan@live.com>
2022-09-27 08:56:43 +0530

    Add: error handling

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,20 +6,27 @@ fn main() {
 
     println!("");
     let secret_number = rand::thread_rng().gen_range(1..=100);
+    // Loop
     loop {
         println!("Please input your number");
 
         // Declare mutable variable guess as string type
         let mut guess = String::new();
 
+        // Read line
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        // Convert string to number 
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {guess}");
 
+        // Match number
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
