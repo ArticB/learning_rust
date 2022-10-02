3c5c6602fc59b8284dfb6883e771dedbf83170b0
Deepraj Pradhan <deeprajpradhan@live.com>
2022-10-02 12:20:34 +0530

    Add: data types

--- rust_concepts/src/main.rs
+++ rust_concepts/src/main.rs
@@ -1,18 +1,37 @@
 // const GLOBAL_CONSTANT:u32 = 5;
 
 fn main() {
-    shadowing2()
+    data_types();
 }
 
-fn shadowing2() {
-    let x = "hello";
+fn data_types() {
+    
+    let int: u128 = 99999999999999;
+    let float: f64 = 0.99999999999999;
+    let char: char = '😊';
+    
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let tup_data1 = tup.0;
 
-    {
-        let x = x.len(); 
-        println!("The length of x string is {x}");
-    }
-    println!("{x}");
+    let array = [0, 1, 2, 3, 4];
+    let array_data = array[0];
+    println!("integer {int}");
+    println!("floating {float}");
+    println!("character {char}");
+    
+    println!("tuple {tup_data1}");
+    println!("array {array_data}");
 }
+
+// fn shadowing2() {
+//     let x = "hello";
+
+//     {
+//         let x = x.len(); 
+//         println!("The length of x string is {x}");
+//     }
+//     println!("{x}");
+// }
 
 // fn shadowing() {
 //     let x = 5;
