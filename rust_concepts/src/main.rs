5a4f2878e14e9387aaea0a1e65e978fcff9931f9
Deepraj Pradhan <deeprajpradhan@live.com>
2022-10-02 12:27:40 +0530

    Add: standard input output

--- rust_concepts/src/main.rs
+++ rust_concepts/src/main.rs
@@ -1,27 +1,42 @@
+use std::io;
+
 // const GLOBAL_CONSTANT:u32 = 5;
 
 fn main() {
-    data_types();
+    std_input_output()
 }
 
-fn data_types() {
-    
-    let int: u128 = 99999999999999;
-    let float: f64 = 0.99999999999999;
-    let char: char = '😊';
-    
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
-    let tup_data1 = tup.0;
+fn std_input_output() {
+    let mut number = String::new();
 
-    let array = [0, 1, 2, 3, 4];
-    let array_data = array[0];
-    println!("integer {int}");
-    println!("floating {float}");
-    println!("character {char}");
-    
-    println!("tuple {tup_data1}");
-    println!("array {array_data}");
+    io::stdin()
+        .read_line(&mut number)
+        .expect("Failed to read line");
+    let number: usize = number
+        .trim()
+        .parse()
+        .expect("Not a number");
+    println!("your number is {number}");
 }
+
+// fn data_types() {
+    
+//     let int: u128 = 99999999999999;
+//     let float: f64 = 0.99999999999999;
+//     let char: char = '😊';
+    
+//     let tup: (i32, f64, u8) = (500, 6.4, 1);
+//     let tup_data1 = tup.0;
+
+//     let array = [0, 1, 2, 3, 4];
+//     let array_data = array[0];
+//     println!("integer {int}");
+//     println!("floating {float}");
+//     println!("character {char}");
+    
+//     println!("tuple {tup_data1}");
+//     println!("array {array_data}");
+// }
 
 // fn shadowing2() {
 //     let x = "hello";
