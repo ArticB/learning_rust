99dc8b0fa410b9df92d31928eee5d2ab662a4bb8
Deepraj Pradhan <deeprajpradhan@live.com>
2022-10-02 11:56:34 +0530

    Add: shadowing

--- rust_concepts/src/main.rs
+++ rust_concepts/src/main.rs
@@ -1,7 +1,18 @@
 // const GLOBAL_CONSTANT:u32 = 5;
 
 fn main() {
+    shadowing()
+}
 
+fn shadowing() {
+    let x = 5;
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x is shadowed by {x}")
+    }
+    println!("The value of x is: {x}")
 }
 
 // fn mutability() {
