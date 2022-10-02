e553d9ea99e6bfd609a491106aa758a8f4d99426
Deepraj Pradhan <deeprajpradhan@live.com>
2022-10-02 11:53:36 +0530

    Add: global constants

--- rust_concepts/src/main.rs
+++ rust_concepts/src/main.rs
@@ -1,10 +1,12 @@
+// const GLOBAL_CONSTANT:u32 = 5;
+
 fn main() {
 
 }
 
-fn mutability() {
-    let mut x = 5;
-    println!("the value of x is: {x}");
-    x = 6;
-    print!("the value of x is: {x}");
-}
+// fn mutability() {
+//     let mut x = 5;
+//     println!("the value of x is: {x}");
+//     x = 6;
+//     print!("the value of x is: {x}");
+// }
